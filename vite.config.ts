33d5233cd5fb21544e4c6f18d7cc4e736199fcd7
ninnaduarte <277296919+ninnaduarte@users.noreply.github.com>
2026-04-18 14:02:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+// https://vitejs.dev
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/ninasite/" : "/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
 }));
